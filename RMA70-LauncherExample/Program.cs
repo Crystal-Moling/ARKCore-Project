b14381742f674e566953e567210cdaa8a7ec8429
Crystal Moling <clovermoling@gmail.com>
2021-10-02 11:26:43 +0800

    Add README

--- RMA70-LauncherExample/Program.cs
+++ RMA70-LauncherExample/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Threading.Tasks;
 using RMA70_LauncherLib.Core.DataProcess.NetworkOperation.Authentication;
 using RMA70_LauncherLib.CoreIO;
@@ -16,18 +15,6 @@ namespace RMA70_LauncherExample
             Console.WriteLine(re.Username);
             Console.WriteLine(re.AccessToken);
             Console.WriteLine(re.Verified);
-            
-            Console.WriteLine("Refresh=============");
-            var re1 = await ygg.RefreshAsync(re.AccessToken);
-            Console.WriteLine(re1.AccessToken);
-            
-            Console.WriteLine("Validate============");
-            Console.WriteLine($"0:{await ygg.ValidateAsync(re.AccessToken)}");
-            Console.WriteLine($"1:{await ygg.ValidateAsync(re1.AccessToken)}");
-            
-            Console.WriteLine("Invalidate==========");
-            await ygg.InvalidateAsync(re1.AccessToken, re1.ClientToken);
-            Console.WriteLine($"Invalidate:{await ygg.ValidateAsync(re1.AccessToken)}");
 
             Console.WriteLine("JavaPath============");
             Console.WriteLine(IO.LocateJava());
